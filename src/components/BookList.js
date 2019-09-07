3341b88e452624c9d10d235686ac98527ef6910c
Somto M.Ugeh <smugeh@gmail.com>
2019-09-07 12:39:59 +0100

    Consumed context using consumer syntax

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class BookList extends Component {
   render() {
     return (
-      <div className="book-list">
-        <ul>
-          <li>The wait of Kings</li>
-          <li>The name of the winds</li>
-          <li>The final empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, dark, light } = context;
+          const theme = isLightTheme ? light : dark;
+
+          return (
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+              <ul>
+                <li style={{ background: theme.ui }}>The wait of Kings</li>
+                <li style={{ background: theme.ui }}>The name of the winds</li>
+                <li style={{ background: theme.ui }}>The final empire</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
